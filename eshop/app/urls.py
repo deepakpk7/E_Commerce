7cfd9537adab22a28a284f66e45063e1c0a2d27e
Deepak P K <137987354+deepakpk7@users.noreply.github.com>
2024-11-14 11:35:01 +0530

    create model and add product

--- eshop/app/urls.py
+++ eshop/app/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     path('',views.e_shop_login),
     path('register',views.register),
-    path('shop_home',views.shop_home)
+    path('shop_home',views.shop_home),
+    path('logout',views.e_shop_logout),
     
 ]
--- eshop/app/views.py
+++ eshop/app/views.py
@@ -4,15 +4,18 @@ from django.contrib import messages
 # Create your views here.
 
 def e_shop_login(req):
+    if 'shop' in req.session:
+        return redirect(shop_home)
     if req.method=='POST':
         uname=req.POST['uname']
         password=req.POST['password']
         data=authenticate(username=uname,password=password)
         if data:
             login(req,data)
+            req.session['shop']=uname  #Create Session
             return redirect(shop_home)
         else:
-            messages.warning(req, "Invalid Username & Password")
+            messages.warning(req, "Invalid Username or Password")
             return redirect(e_shop_login)
     else:
         return render(req,'login.html')
@@ -21,5 +24,13 @@ def e_shop_login(req):
 def register(req):
     return render(req,'register.html')
 
+def e_shop_logout(req):
+    logout(req)
+    req.session.flush() #Delete session
+    return redirect(e_shop_login)
+
 def shop_home(req):
-    return render(req,'shop/home.html')
+    if 'shop' in req.session:
+        return render(req,'shop/home.html')
+    else:
+        return redirect(e_shop_login)
