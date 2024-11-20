fcaead0aa7384fbedbd1cf45b2e5740450a1e7c4
Deepak P K <137987354+deepakpk7@users.noreply.github.com>
2024-11-20 11:34:37 +0530

    add user registration and user home page

--- eshop/app/urls.py
+++ eshop/app/urls.py
@@ -4,9 +4,13 @@ from . import views
 
 urlpatterns = [
     path('',views.e_shop_login),
-    path('register',views.register),
     path('shop_home',views.shop_home),
     path('add_pro',views.add_product),
     path('logout',views.e_shop_logout),
     path('edit_product/<pid>',views.edit_product),
+    path('delete_product/<pid>',views.delete_product),
+# -------------user----------
+    path('register',views.register),
+    path('user_home',views.user_home)
+
 ]
--- eshop/app/views.py
+++ eshop/app/views.py
@@ -2,29 +2,34 @@ from django.shortcuts import render,redirect
 from django.contrib.auth import authenticate,login,logout
 from django.contrib import messages
 from .models import *
+import os
+from django.contrib.auth.models import User
 # Create your views here.
 
 def e_shop_login(req):
     if 'shop' in req.session:
         return redirect(shop_home)
+    if 'user' in req.session:
+        return redirect(user_home)
     if req.method=='POST':
         uname=req.POST['uname']
         password=req.POST['password']
         data=authenticate(username=uname,password=password)
         if data:
             login(req,data)
-            req.session['shop']=uname  #Create Session
-            return redirect(shop_home)
+            if data.is_superuser:
+                req.session['shop']=uname  #Create Session
+                return redirect(shop_home)
+            else:
+                req.session['user']=uname
+                return redirect(user_home)
         else:
             messages.warning(req, "Invalid Username or Password")
             return redirect(e_shop_login)
     else:
         return render(req,'login.html')
     
-
-def register(req):
-    return render(req,'register.html')
-
+# -------------------------admin-------------
 
 def shop_home(req):
     if 'shop' in req.session:
@@ -85,3 +90,36 @@ def edit_product(req,pid):
     else:
         data=Product.objects.get(pk=pid)
         return render(req,'shop/edit.html',{'data':data})
+    
+def delete_product(req,pid):
+    data=Product.objects.get(pk=pid)
+    file=data.img.url
+    file=file.split('/')[-1]
+    os.remove('media/'+file)
+    data.delete()
+    return redirect(shop_home)
+
+# ---------------------------user------------------
+
+def register(req):
+    if req.method=='POST':
+        uname=req.POST['uname']
+        email=req.POST['email']
+        pswd=req.POST['pswd']
+        try:
+            data=User.objects.create_user(first_name=uname,email=email,
+                                        username=email,password=pswd)
+            data.save()
+        except:
+            messages.warning(req, "Username or Email already exist")
+            return redirect(register)
+        return redirect(e_shop_login)
+    else:
+        return render(req,'user/register.html')
+
+def user_home(req):
+    if 'user' in req.session:
+        data=Product.objects.all()
+        return render(req,'user/home.html',{'products':data})
+    else:
+        return redirect(e_shop_login)
